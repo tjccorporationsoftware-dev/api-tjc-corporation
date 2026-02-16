1b5989753daa99f797e57a044356ecb439b9b657
Padungdech Khamdaeng <std65122710112-cell@example.com>
2026-02-16 14:06:36 +0700

    h

--- server.js
+++ server.js
@@ -534,6 +534,6 @@ app.put("/api/site/contact", authRequired, adminRequired, async (req, res) => {
 });
 
 // 10. Start Server
-app.listen(PORT, () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
